7d1a22f444f1c1b23860b0fc69372d193aa80a94
Elem <elem15@yandex.ru>
2023-03-15 19:54:23 +0300

    feat: add Query

--- lesson-03/src/App.jsx
+++ lesson-03/src/App.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Details from './Details';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity
+    }
+  }
+});
 import SearchParams from "./SearchParams";
 const App = () => {
   return (
     <BrowserRouter>
-      <header><Link to="/">Adopt me</Link> </header>
-
-      <Routes>
-        <Route path="/" element={<SearchParams />} />
-        <Route path="/details/:id" element={<Details />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header><Link to="/">Adopt me</Link> </header>
+        <Routes>
+          <Route path="/" element={<SearchParams />} />
+          <Route path="/details/:id" element={<Details />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
--- lesson-03/src/Details.jsx
+++ lesson-03/src/Details.jsx
@@ -1,10 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
+import fetchPet from './fetchPet';
 
 const Details = () => {
   const { id } = useParams();
+  const result = useQuery(['details', id], fetchPet);
+
+  if (result.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    );
+  }
+  const [pet] = result.data.pets;
   return (
-    <div>Details = {id}</div>
-  );
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
+        <button>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+      </div>
+    </div>);
 };
 
 export default Details;
