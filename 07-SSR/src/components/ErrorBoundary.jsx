bbab5908c45aa16847c1f38358bf3f43c448ae35
Elem <elem15@yandex.ru>
2023-03-20 15:14:22 +0300

    feat: add SSR with Express

---
+++ 07-SSR/src/components/ErrorBoundary.jsx
@@ -0,0 +1,24 @@
+import { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+  // this method catch an error
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+  componentDidCatch(error, info) {
+    console.error('error boundary has an error', error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <h2>There was an error with this listing. Go back to <Link to="/">Home page</Link></h2 >
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
