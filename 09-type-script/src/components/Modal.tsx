1b15f13dea5984c80ffcd2ea57b6d637cca3b77c
Elem <elem15@yandex.ru>
2023-03-21 19:39:02 +0300

    feat: add type-script config & 1 file convert

---
+++ 09-type-script/src/components/Modal.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { useRef, useEffect, MutableRefObject, ReactElement } from "react";
+import { createPortal } from "react-dom";
+interface IProps {
+  children: ReactElement,
+  renderModal: () => void
+}
+const Modal = ({ children, renderModal }: IProps) => {
+  const divRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
+  if (!divRef.current) {
+    divRef.current = document.createElement("div");
+  }
+  const handleClick = () => {
+    renderModal();
+  };
+  useEffect(() => {
+    const rootEl = document.getElementById("modal");
+    if (!rootEl || !divRef.current) {
+      return;
+    }
+    rootEl.appendChild(divRef.current);
+    rootEl.addEventListener("click", () => handleClick());
+    return () => {
+      if(divRef.current) rootEl.removeChild(divRef.current);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return createPortal(
+    <div
+      onClick={(event) => {
+        event.stopPropagation();
+      }}
+    >
+      {children}
+    </div>,
+    divRef.current
+  );
+};
+
+export default Modal;
---
+++ 09-type-script/src/hooks/useBreeds.js
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import fetchBreeds from "../queries/fetchBreeds";
+
+const useBreeds = (animal) => {
+  const results = useQuery(["breeds", animal], fetchBreeds);
+  return [results?.data?.breeds ?? [], results.status];
+};
+
+export default useBreeds;
