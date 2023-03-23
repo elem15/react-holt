0fdd33e5f7d1999477c4f81f93c92d6bab97c849
Elem <elem15@yandex.ru>
2023-03-23 18:08:46 +0300

    feat: test to Pet & Carousel

---
+++ 11-test/src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useContext, useState } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Details from "./components/Details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import SearchParams from "./components/SearchParams";
+import AdoptPetContext from './AdoptPetContext';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+const App = () => {
+  const adoptPet = useState();
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AdoptPetContext.Provider value={adoptPet}>
+          <header>
+            <Link to="/">Adopt me</Link>{" "}
+          </header>
+          <Routes>
+            <Route path="/" element={<SearchParams />} />
+            <Route path="/details/:id" element={<Details />} />
+          </Routes>
+        </AdoptPetContext.Provider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(React.createElement(App));
---
+++ 11-test/src/components/Pets.jsx
@@ -0,0 +1,20 @@
+import Pet from "./Pet";
+
+const Pets = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h2>Pets not found</h2>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            {...pet} //not good idea throw all data in component
+            key={pet.id}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Pets;
