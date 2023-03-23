4a942f8ad0e5612e990c5763e6b506c9a6ec721e
Elem <elem15@yandex.ru>
2023-03-23 20:30:55 +0300

    feat: test to query

--- 11-test/src/__tests__/useBreedsHook.test.jsx
+++ 11-test/src/__tests__/useBreedsHook.test.jsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import { expect, test } from "vitest";
 import useBreeds from "../hooks/useBreeds";
 
@@ -22,4 +22,28 @@ test("give empty list with no animal", () => {
   const [breeds, status] = result.current;
   expect(breeds).toHaveLength(0);
   expect(status).toBe("loading");
+});
+test("give back breeds with an animal", async () => {
+  const breeds = [
+    "Havanese",
+    "Bichon Frise",
+    "Poodle",
+    "Maltese",
+    "Golden Retriever",
+    "Labrador",
+    "Husky",
+  ];
+  fetch.mockResponseOnce(
+    JSON.stringify({
+      animal: "dog",
+      breeds,
+    })
+  );
+  const { result } = renderHook(() => useBreeds("dog"), {
+    wrapper: ({ children }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    ),
+  });
+  await waitFor(() => expect(result.current[1]).toBe("success"));
+  expect(result.current[0]).toEqual(breeds);
 });
